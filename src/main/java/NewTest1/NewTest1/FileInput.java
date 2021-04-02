856eb7d10e7afd74cc3adb9f04160543642c9f7b
mathan <mmathan39@gmail.com>
2021-04-02 11:37:39 +0530

    csvtoExcel

--- src/main/java/NewTest1/NewTest1/FileInput.java
+++ src/main/java/NewTest1/NewTest1/FileInput.java
@@ -1,21 +1,62 @@
 package NewTest1.NewTest1;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.util.Scanner;
 
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+
+
+
+
 public class FileInput {
+	
+	public static void csvToXLSX() {
+	    try {
+	        String csvFileAddress = "W:\\CSVDemo.csv"; //csv file address
+	        String xlsxFileAddress = "W:\\test.xlsx"; //xlsx file address
+	        XSSFWorkbook workBook = new XSSFWorkbook();
+	        XSSFSheet sheet = workBook.createSheet("sheet1");
+	        String currentLine=null;
+	        int RowNum=0;
+	        BufferedReader br = new BufferedReader(new FileReader(csvFileAddress));
+	        while ((currentLine = br.readLine()) != null) {
+	            String str[] = currentLine.split(",");
+	            RowNum++;
+	            XSSFRow currentRow=sheet.createRow(RowNum);
+	            for(int i=0;i<str.length;i++){
+	                currentRow.createCell(i).setCellValue(str[i]);
+	            }
+	        }
+
+	        FileOutputStream fileOutputStream =  new FileOutputStream(xlsxFileAddress);
+	        workBook.write(fileOutputStream);
+	        fileOutputStream.close();
+	        System.out.println("Done");
+	    } catch (Exception ex) {
+	        System.out.println(ex.getMessage()+"Exception in try");
+	    }
+	}
+
 
 	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 		
-		Scanner sc = new Scanner(new File("W:\\CSVDemo.csv"));  
+		csvToXLSX();
+		
+	/*	Scanner sc = new Scanner(new File("W:\\CSVDemo.csv"));  
 		sc.useDelimiter(",");     
 		while (sc.hasNext())    
 		{  
 		System.out.print(sc.next());    
 		}   
-		sc.close();    
+		sc.close();*/    
 
 	}
 
